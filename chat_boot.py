605dc456567af9fc204c56af752cc185a0bbea0e
xelastone <126184753+lalolanda11@users.noreply.github.com>
2023-07-27 12:01:15 -0600

    Update chat_boot.py

--- chat_boot.py
+++ chat_boot.py
@@ -1,14 +1,24 @@
 import openai
 import os
+import base
+basedatos=dato()                                                                                                                                                                                                   
 print(os.getcwd())
-api="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"
+#CXod
+#Aqui va la api de openai 
+#Si no tienes un api, debes de registrarte en la pagina oficial de openai pára tener un api
+#Ingresa el api en la variable llamada api
+api=""
 openai.api_key=api
 #dato=input('Dato ')
 chat=[]
 while True:
+    #Puedes cambiar el nombre de la entrada 
     pregunta=input('Xelastone@archroot \n $ ')
     if pregunta == 'c' or pregunta == "quit":
             break
+    elif pregunta == "quien creo el codigo" or pregunta == "cual es mi nombre":
+        os.system("termux-tts-speak Este codigo fue creado por xela stone y mi nombre es elena stone")
+        os.system("clear")
     else:
 
 
